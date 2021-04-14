ee92d82c5f3dd99f63c802699b3d32a792c0ac4a
saitoSay <seinyacchi@gmail.com>
2021-04-14 13:39:46 +0900

    マップ用テキストデータを読み込んでint型の二次元配列に変換し、TIlemapで敷き詰める準備をした

---
+++ Assets/scripts/CreateMap.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CreateMap : MonoBehaviour
+{
+    [SerializeField] int[,] m_mapData;
+    [SerializeField] string m_fileName;
+    void Start()
+    {
+
+        SpritMapData();
+        //inctance.Load(m_fileName);
+    }
+
+    void Update()
+    {
+        
+    }
+    public void DataMapping()
+    {
+        MapStates mapStates;
+        //switch (mapStates)
+        //{
+        //    case MapStates.Road:
+        //        break;
+        //    case MapStates.Wall:
+        //        break;
+        //    case MapStates.Start:
+        //        break;
+        //    case MapStates.Goal:
+        //        break;
+        //}
+    }
+    public int[,] SpritMapData()
+    {
+        TextFileCriate instance = new TextFileCriate();
+        string text = instance.LoadTextResource("mapData");
+        string[] lineText = text.Split('\n');
+        m_mapData = new int[lineText.Length, 9];
+        for (int i = 0; i < lineText.Length; i++)
+        {
+            string[] pointText = lineText[i].Split(',');
+            for (int j = 0; j < pointText.Length; j++)
+            {
+                m_mapData[i,j] = int.Parse(pointText[j]);
+                Debug.Log(m_mapData[i, j]);
+            }
+        }
+        return m_mapData;
+    }
+    enum MapStates
+    {
+        Road,
+        Wall,
+        Start,
+        Goal
+    }
+}
