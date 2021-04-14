8f052464551811ed6173383e6f05cfa0d75ec88a
nitta <yosshi3800@gmail.com>
2021-04-14 16:27:41 +0900

    SceneLoder.csシーンを変更できるスクリプトを作りました。

---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーンロードスクリプト
+/// </summary>
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] string scenename = "";
+
+    /// <summary>
+    /// シーンを変える
+    /// </summary>
+    /// <param name="scenename">変えたいシーン名</param>
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
